b1d1567a4d97374b024b528007d1ea31361f9f41
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-01-31 16:55:46 -0200

    Simple Stub Test API

--- OHTTStubPocTests/OHTTStubPocTests.swift
+++ OHTTStubPocTests/OHTTStubPocTests.swift
@@ -7,13 +7,14 @@
 //
 
 import XCTest
+import OHHTTPStubs
 @testable import OHTTStubPoc
 
 class OHTTStubPocTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
     
     override func tearDown() {
@@ -22,15 +23,75 @@ class OHTTStubPocTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let response = [
+            "username" : "feh.wilinando",
+            "password" : "123"
+                         ]
+        
+        stub(condition: isHost("api.com.br") && isScheme("http")) { _ in
+            return OHHTTPStubsResponse(jsonObject: response, statusCode: 200, headers: .none)
         }
+        
+        
+        let session = URLSession.shared
+        
+        guard let url = URL(string: "http://api.com.br") else {
+            XCTFail("Error on build URL")
+            return
+        }
+        
+        let expect = expectation(description: "Calls the callback with a resource object")
+        
+        let task = session.dataTask(with: url) { (data, urlresponse, error) in
+            
+            
+            guard let httpResponse = urlresponse as? HTTPURLResponse else {
+                XCTFail("Cast to HTTPURLResponse fail")
+                return
+            }
+            
+            XCTAssertEqual(200, httpResponse.statusCode)
+            
+            guard error == nil else {
+                XCTFail("Error \(error!.localizedDescription)")
+                return
+            }
+            
+            guard let data = data  else {
+                XCTFail("Data Fail")
+                return
+            }
+            
+            
+            
+            print(String(data: data, encoding: String.Encoding.utf8)!)
+            
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+                XCTFail("Json Fail")
+                return
+            }
+            
+            guard let result = json as? [String: String] else {
+                XCTFail("Cast Dictonary Fail")
+                return
+            }
+            
+            
+            XCTAssertTrue(result.keys.contains("username"))
+            XCTAssertTrue(result.keys.contains("password"))
+            XCTAssertEqual("feh.wilinando", result["username"])
+            XCTAssertEqual("123", result["password"])
+            
+            expect.fulfill()
+        }
+        
+        task.resume()
+        
+        wait(for: [expect], timeout: 0.3)
+        
+        
+        OHHTTPStubs.removeAllStubs()
+        
     }
-    
+
 }
